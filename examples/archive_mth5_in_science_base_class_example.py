5f0b20c0c8712a2d92b7ebe88a619fb19efa35a4
JP <peacock.jared@gmail.com>
2021-06-24 15:49:20 -0700

    Update archive_mth5_in_science_base_class_example.py

--- examples/archive_mth5_in_science_base_class_example.py
+++ examples/archive_mth5_in_science_base_class_example.py
@@ -35,6 +35,7 @@ sbmt.survey_csv_fn = Path(
     r"c:\Users\jpeacock\Documents\test_data\Archive\survey_summary.csv"
 )
 
+# )
 # path to mth5 configuration file
 # this is a configuration file that has metadata explaining most of the
 # common information needed by the user.  See example files
